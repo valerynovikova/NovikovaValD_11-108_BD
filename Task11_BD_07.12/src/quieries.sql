ec903cb63cdfcfd8b12205903827fcd992bcd50e
unknown <valerianovikova06608@gmail.com>
2022-12-07 11:10:03 +0300

    Task added

---
+++ Task11_BD_07.12/src/quieries.sql
@@ -0,0 +1,23 @@
+
+EXPLAIN ANALYZE select orders.name_driver, Max(scores.value)
+                from driver
+                         join orders on orders.id = driver.id
+                         join scores on scores.driver_id = driver.id
+                where driver.rating = 5
+                group by orders.name_driver;
+
+
+--2
+EXPLAIN ANALYZE select orders.name_driver, Max(scores.value)
+                from driver
+                         join orders on orders.id = driver.id
+                         join scores on scores.driver_id = driver.id
+                group by orders.name_driver;
+
+-- 3
+CREATE INDEX INDEX_table ON orders USING HASH (number_car);
+
+-- 4
+
+CREATE EXTENSION INDEX_table2;
+CREATE INDEX INDEX_table3 ON driver USING gin (name_driver);
